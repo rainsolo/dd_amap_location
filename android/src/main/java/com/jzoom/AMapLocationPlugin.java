d44cb844d130a5a48b78bf9f02c41f2bc9d06db9
rainsolo <fanzhu@imdada.cn>
2019-11-04 17:20:21 +0800

    [fix]rename file name

--- android/src/main/java/com/jzoom/AMapLocationPlugin.java
+++ android/src/main/java/com/jzoom/AMapLocationPlugin.java
@@ -113,7 +113,7 @@ public class AMapLocationPlugin implements MethodCallHandler {
 
     private Map resultToMap(AMapLocation a) {
 
-        Map map = new HashMap();
+        Map<String, Object> map = new HashMap<>();
 
         if (a != null) {
 
@@ -243,25 +243,27 @@ public class AMapLocationPlugin implements MethodCallHandler {
      * <p>
      * <p>
      * this.geoLanguage : GeoLanguage.DEFAULT,
-     *
-     * @param arguments
-     * @return
      */
     private void parseOptions(AMapLocationClientOption option, Map arguments) {
         //  AMapLocationClientOption option = new AMapLocationClientOption();
         option.setLocationMode(AMapLocationClientOption.AMapLocationMode.valueOf((String) arguments.get("locationMode")));//可选，设置定位模式，可选的模式有高精度、仅设备、仅网络。默认为高精度模式
-        option.setGpsFirst((Boolean) arguments.get("gpsFirst"));//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
-        option.setHttpTimeOut((Integer) arguments.get("httpTimeOut"));//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
-        option.setInterval((Integer) arguments.get("interval"));//可选，设置定位间隔。默认为2秒
-        option.setNeedAddress((Boolean) arguments.get("needsAddress"));//可选，设置是否返回逆地理地址信息。默认是true
-        option.setOnceLocation((Boolean) arguments.get("onceLocation"));//可选，设置是否单次定位。默认是false
-        option.setOnceLocationLatest((Boolean) arguments.get("onceLocationLatest"));//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
+        option.setGpsFirst((Boolean) getOrDefault(arguments, "gpsFirst", false));//可选，设置是否gps优先，只在高精度模式下有效。默认关闭
+        option.setHttpTimeOut((Integer) getOrDefault(arguments, "httpTimeOut", 30));//可选，设置网络请求超时时间。默认为30秒。在仅设备模式下无效
+        option.setInterval((Integer) getOrDefault(arguments, "interval", 2));//可选，设置定位间隔。默认为2秒
+        option.setNeedAddress((Boolean) getOrDefault(arguments, "needsAddress", true));//可选，设置是否返回逆地理地址信息。默认是true
+        option.setOnceLocation((Boolean) getOrDefault(arguments, "onceLocation", false));//可选，设置是否单次定位。默认是false
+        option.setOnceLocationLatest((Boolean) getOrDefault(arguments, "onceLocationLatest", false));//可选，设置是否等待wifi刷新，默认为false.如果设置为true,会自动变为单次定位，持续定位时不要使用
         AMapLocationClientOption.setLocationProtocol(AMapLocationClientOption.AMapLocationProtocol.valueOf((String) arguments.get("locationProtocal")));//可选， 设置网络请求的协议。可选HTTP或者HTTPS。默认为HTTP
-        option.setSensorEnable((Boolean) arguments.get("sensorEnable"));//可选，设置是否使用传感器。默认是false
-        option.setWifiScan((Boolean) arguments.get("wifiScan")); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
-        option.setLocationCacheEnable((Boolean) arguments.get("locationCacheEnable")); //可选，设置是否使用缓存定位，默认为true
+        option.setSensorEnable((Boolean) getOrDefault(arguments, "sensorEnable", false));//可选，设置是否使用传感器。默认是false
+        option.setWifiScan((Boolean) getOrDefault(arguments, "wifiScan", true)); //可选，设置是否开启wifi扫描。默认为true，如果设置为false会同时停止主动刷新，停止以后完全依赖于系统刷新，定位位置可能存在误差
+        option.setLocationCacheEnable((Boolean) getOrDefault(arguments, "locationCacheEnable", true)); //可选，设置是否使用缓存定位，默认为true
         option.setGeoLanguage(AMapLocationClientOption.GeoLanguage.valueOf((String) arguments.get("geoLanguage")));//可选，设置逆地理信息的语言，默认值为默认语言（根据所在地区选择语言）
-
     }
 
+    private Object getOrDefault(Map map, Object key, Object defaultValue) {
+        Object value = map.get(key);
+        if (value == null)
+            value = defaultValue;
+        return value;
+    }
 }
